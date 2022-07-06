3d90048a518d09e9bec89ca9d768eb2140d80e41
Elem <elem15@yandex.ru>
2022-07-06 13:05:21 +0300

    feat: register query

--- src/index.ts
+++ src/index.ts
@@ -4,25 +4,26 @@ const { buildSchema } = require('graphql');
 require('dotenv').config()
 
 const axios = require('axios').default;
-const baseURL = process.env.GENRES_URL;
+const genresURL = process.env.GENRES_URL;
+const usersURL = process.env.USERS_URL;
 const PORT = process.env.PORT || 4000;
-// Make a request for a user with a given ID
 
-axios.get("http://localhost:3001/v1/genres")
-  .then(function (response: Response): void {
-    // handle success
+interface ExtResponse extends Response {
+  data: object
+}
+axios.post(`${usersURL}/register`, {
+  firstName: 'first name',
+  lastName: 'last name',
+  password: 'last name',
+  email: 'dfjak@gmail.com'
+})
+.then(function (response: ExtResponse) {
+  console.log(response.data);
+})
+.catch(function (error: Error) {
+  console.log(error);
+});
 
-    // console.log(response.data);
-  })
-  .catch(function (error: Error) {
-
-    // handle error
-    console.log(error);
-  })
-  .then(function () {
-    // always executed
-  });
-// Construct a schema, using GraphQL schema language
 const schema = buildSchema(`
   type Genre {
     _id: ID!
@@ -33,9 +34,23 @@ const schema = buildSchema(`
     subGenres: [Genre] 
   }
 
+  type User {
+    _id: ID!
+    firstName: String
+    lastName: String
+    password: String
+    email: String
+  }
+
   type Query {
     genre(id: ID!): Genre
-    genres(limit: Int, offset: Int): [Genre]    
+    genres(limit: Int, offset: Int): [Genre]
+    register( 
+      firstName: String,
+      lastName: String,
+      password: String,
+      email: String      
+      ): User   
   }
   
   type Mutation {
@@ -44,19 +59,40 @@ const schema = buildSchema(`
   `);
   
 // The root provides a resolver function for each API endpoint
+type User = {
+  firstName: string;
+  lastName: string;
+  password: string;
+  email: string;
+}
 const root = {
   // limit: async () => {
   //   const response = await axios.get("http://localhost:3001/v1/genres");
   //   return response.data.limit;
   // },
   genres: async (limit: number, offset: number) => {
-    const response = await axios.get(baseURL);
+    const response = await axios.get(genresURL);
     return response.data.items;
   },
-  genre: async (id: {id: string}) => {
-    const response = await axios.get(baseURL);
-    return response.data.items.find((item: {_id: string})  => item._id === id.id);
+  genre: async ({id}: {id: string}) => {
+    const response = await axios.get(genresURL);
+    return response.data.items.find((item: {_id: string}) => item._id === id);
   },
+  register: async ({
+    firstName,
+    lastName,
+    password,
+    email
+  }: User) => {
+      const response = await axios.post(`${usersURL}/register`, {
+        firstName,
+        lastName,
+        password,
+        email
+      });
+      console.log(firstName);
+      return response.data;
+    }
 };
 
 const app = express();
