a8020e0e5524e206899fd7be11143c983c9bb533
Elem <elem15@yandex.ru>
2022-07-06 14:56:38 +0300

    feat: implement authentication with jwt & first mutation

--- src/index.ts
+++ src/index.ts
@@ -8,22 +8,6 @@ const genresURL = process.env.GENRES_URL;
 const usersURL = process.env.USERS_URL;
 const PORT = process.env.PORT || 4000;
 
-interface ExtResponse extends Response {
-  data: object
-}
-axios.post(`${usersURL}/register`, {
-  firstName: 'first name',
-  lastName: 'last name',
-  password: 'last name',
-  email: 'dfjak@gmail.com'
-})
-.then(function (response: ExtResponse) {
-  console.log(response.data);
-})
-.catch(function (error: Error) {
-  console.log(error);
-});
-
 const schema = buildSchema(`
   type Genre {
     _id: ID!
@@ -32,6 +16,10 @@ const schema = buildSchema(`
     country: String
     year: String
     subGenres: [Genre] 
+  }
+
+  type JWT {
+    jwt: String
   }
 
   type User {
@@ -51,32 +39,56 @@ const schema = buildSchema(`
       password: String,
       email: String      
       ): User   
+    login(    
+      password: String,
+      email: String      
+      ): JWT   
   }
   
   type Mutation {
     createGenre(name: String, description: String, country: String, year: Int): Genre
   }
   `);
-  
-// The root provides a resolver function for each API endpoint
+
+interface ExtResponse extends Response {
+  data: object
+}
+
 type User = {
   firstName: string;
   lastName: string;
   password: string;
   email: string;
 }
+
+type Genre = {
+  name: string
+  description: string
+  country: String
+  year: number
+}
+
+let token = '';
+
 const root = {
-  // limit: async () => {
-  //   const response = await axios.get("http://localhost:3001/v1/genres");
-  //   return response.data.limit;
-  // },
   genres: async (limit: number, offset: number) => {
     const response = await axios.get(genresURL);
     return response.data.items;
   },
-  genre: async ({id}: {id: string}) => {
+  genre: async ({ id }: { id: string }) => {
     const response = await axios.get(genresURL);
-    return response.data.items.find((item: {_id: string}) => item._id === id);
+    return response.data.items.find((item: { _id: string }) => item._id === id);
+  },
+  createGenre: async ({ name, description, country, year }: Genre) => {
+    const response = await axios.post(genresURL,
+      { name, description, country, year },
+      {
+        headers: {
+          'Authorization': `Basic ${token}`
+        }
+      });
+    console.log(response.data)
+    return response.data;
   },
   register: async ({
     firstName,
@@ -84,15 +96,25 @@ const root = {
     password,
     email
   }: User) => {
-      const response = await axios.post(`${usersURL}/register`, {
-        firstName,
-        lastName,
-        password,
-        email
-      });
-      console.log(firstName);
-      return response.data;
-    }
+    const response = await axios.post(`${usersURL}/register`, {
+      firstName,
+      lastName,
+      password,
+      email
+    });
+    return response.data;
+  },
+  login: async ({
+    password,
+    email
+  }: User) => {
+    const response = await axios.post(`${usersURL}/login`, {
+      password,
+      email
+    });
+    token = response.data.jwt
+    return response.data;
+  }
 };
 
 const app = express();
